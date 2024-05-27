de3beb084cb66bf916820bbad59419f602c0552d
jaimesansondosa <130551474+jaimesansondosa@users.noreply.github.com>
2024-05-27 11:33:30 +0100

    Update LadderMovement

--- Assets/Scripts/LadderMovement.cs
+++ Assets/Scripts/LadderMovement.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
@@ -18,7 +18,7 @@ public class LadderMovement : MonoBehaviour
     private void Start()
     {
         playerMovement = FindObjectOfType<PlayerMovement>();
-        //player = playerMovement.rb;
+        player = playerMovement.rb;
     }
 
     // Update is called once per frame
@@ -39,6 +39,63 @@ public class LadderMovement : MonoBehaviour
             player.gravityScale = 0f;
             player.velocity = new Vector2(player.velocity.x, vertical * climbSpeed); // AVANZAMOS EN EL EJE Y
         } else
+        {
+            player.gravityScale = 2f;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isLadder = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isLadder = false;
+            isClimbing = false;
+        }
+    }
+}
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class LadderMovement : MonoBehaviour
+{
+    [SerializeField] private float climbSpeed = 4f;
+    private float vertical;
+    private bool isLadder;
+    private bool isClimbing;
+
+    [SerializeField] private Rigidbody2D player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        vertical = Input.GetAxis("Vertical"); // ALMACENO LA VELOCIDAD DEL EJE Y
+
+        if (isLadder && math.abs(vertical) > 0f) // SI ES MAYOR DE 0 SIGNIFICA QUE SE PRESIONA W
+        {
+            isClimbing = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isClimbing)
+        {
+            player.gravityScale = 0f;
+            player.velocity = new Vector2(player.velocity.x, vertical * climbSpeed); // AVANZAMOS EN EL EJE Y
+        }
+        else
         {
             player.gravityScale = 2f;
         }
