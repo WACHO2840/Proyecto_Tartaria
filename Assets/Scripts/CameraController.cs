46d567c2728480558a2c04974ee6ea1357b55684
jaimesansondosa <130551474+jaimesansondosa@users.noreply.github.com>
2024-05-29 12:13:56 +0100

    Fondo 1.1

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,8 +7,17 @@ public class CameraController : MonoBehaviour
     [SerializeField] Transform player; // JUGADOR
     [SerializeField] float minHeight, maxHeight, minWidth, maxWidth; // LIMITES DE LA CAMARA
 
+    public Transform farBackGround;
+    private float lastXPos;
+
+    void Start()
+    {
+        //lastXPos
+    }
     void Update()
     {
         transform.position = new Vector3(Mathf.Clamp(player.position.x, minWidth, maxWidth), Mathf.Clamp(player.position.y, minHeight, maxHeight), transform.position.z); // FIJAR CAMARA AL JUGADOR EN EL EJE X E Y
+        
+        farBackGround.position = transform.position;
     }
 }
