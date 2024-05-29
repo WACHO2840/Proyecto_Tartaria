5f51ea8e56310308ce40633b7dfc1b98fd4bc2db
jaimesansondosa <130551474+jaimesansondosa@users.noreply.github.com>
2024-05-29 16:39:14 +0100

    Fondos Niveles

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,19 +10,44 @@ public class CameraController : MonoBehaviour
     private Transform player; // POSICION DEL JUGADOR
     #endregion
 
+    // Nuevas variables para el fondo
+    public Transform farBackGround; // Referencia al fondo lejano
+    private Vector3 previousCameraPosition;
+
+    // Factores de desplazamiento individuales para X y Y
+    [SerializeField] private float parallaxEffectMultiplierX = 0.5f;
+    [SerializeField] private float parallaxEffectMultiplierY = 0.5f;
+
     private void Start()
     {
         playerMovement = FindObjectOfType<PlayerMovement>();
         player = playerMovement.transform;
+
+        // Inicializar la posición anterior de la cámara
+        previousCameraPosition = transform.position;
     }
 
     void Update()
     {
         if (player != null) // Comprobar que el jugador exista
         {
-            transform.position = new Vector3(Mathf.Clamp(player.position.x, minWidth, maxWidth),
-                                             Mathf.Clamp(player.position.y, minHeight, maxHeight),
-                                             transform.position.z); // FIJAR CAMARA AL JUGADOR EN EL EJE X E Y
+            Vector3 newCameraPosition = new Vector3(
+                Mathf.Clamp(player.position.x, minWidth, maxWidth),
+                Mathf.Clamp(player.position.y, minHeight, maxHeight),
+                transform.position.z
+            );
+
+            // Calcular la cantidad de movimiento en X y Y
+            Vector3 deltaMovement = newCameraPosition - previousCameraPosition;
+
+            // Aplicar los factores de desplazamiento al fondo
+            farBackGround.position += new Vector3(deltaMovement.x * parallaxEffectMultiplierX, deltaMovement.y * parallaxEffectMultiplierY, 0f);
+
+            // Actualizar la posición anterior de la cámara
+            previousCameraPosition = newCameraPosition;
+
+            // Actualizar la posición de la cámara
+            transform.position = newCameraPosition;
         }
     }
 }
