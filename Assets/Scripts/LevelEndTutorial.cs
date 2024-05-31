ab27ef2cd2bafba0dbf44f259bd0ef6fd989f62a
WACHO <francisco.sanchez.dosa@gmail.com>
2024-05-31 10:55:27 +0200

    Cambios generales
    - Falta asignar pantalla final al matar al jefe.
    - Error al pasar de pantalla el personaje muere.

---
+++ Assets/Scripts/LevelEndTutorial.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelEndTutorial : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("LevelEnd"))
+        {
+                SceneManager.LoadScene(0); // Vuelta al menu principal
+        }
+    }
+}
