44993cab04bf1cf9b4bddfa1ea8558a491adaba9
WACHO <francisco.sanchez.dosa@gmail.com>
2024-06-03 09:16:09 +0200

    Animación arma
    Animación asociada a ataque y rebaje de variables y funciones en la clase.

--- Assets/Scripts/ArmasGame.cs
+++ Assets/Scripts/ArmasGame.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 
 public class ArmasGame : MonoBehaviour
 {
-    public int damage = 10;  // Cantidad de daño que hace la katana
     public Transform gfx;  // Referencia al transform del gráfico de la katana
     public Transform hitSpot; // Punto de impacto de la katana
-    public float range = 1.0f;  // Alcance del ataque de la katana
     public float maxCooldownTime = 1.0f;  // Tiempo de cooldown para la animación
 
     private bool isAttacking = false;
-    private bool isEquipped = false;  // Indica si la katana está equipada
+    private bool isEquipped = false;  // Indica si el arma esta equipada 
 
     void Start()
     {
         // Verifica si el objeto tiene el tag "katana" y está equipado por el jugador
-        if (gameObject.CompareTag("Katana"))
+        if (gameObject.CompareTag("Katana") || gameObject.CompareTag("Mazo"))
         {
             isEquipped = true;
         }
@@ -25,7 +23,7 @@ public class ArmasGame : MonoBehaviour
     void Update()
     {
         // Detectar la entrada "Fire1" (por defecto, clic izquierdo) y realizar la animación de ataque si está equipada
-        if (Input.GetButtonDown("Fire1") && isEquipped && !isAttacking)
+        if (Input.GetKeyDown(KeyCode.F) && isEquipped && !isAttacking)
         {
             OnActivate();
         }
@@ -33,23 +31,7 @@ public class ArmasGame : MonoBehaviour
 
     private void OnActivate()
     {
-        Debug.Log("Activando ataque");
         StartCoroutine(Animate());
-
-        Collider2D[] targets = Physics2D.OverlapCircleAll(hitSpot.position, range);
-
-        foreach (var target in targets)
-        {
-            if (target.CompareTag("Enemy"))
-            {
-                EnemyStats enemyStats = target.GetComponent<EnemyStats>();
-                if (enemyStats != null)
-                {
-                    // enemyStats.TakeDamage(damage);
-                    Debug.Log("Daño aplicado al enemigo.");
-                }
-            }
-        }
     }
 
     IEnumerator Animate()
@@ -61,7 +43,7 @@ public class ArmasGame : MonoBehaviour
         gfx.localRotation = Quaternion.Euler(0, 0, -70);
         Debug.Log("Katana rotada a -70 grados");
 
-        // Esperar por un tiempo antes de revertir la rotación
+        // Esperar por un  antes de revertir la rotación
         yield return new WaitForSeconds(maxCooldownTime * 0.9f);
 
         // Revertir la rotación de la katana
@@ -73,15 +55,5 @@ public class ArmasGame : MonoBehaviour
 
         isAttacking = false;
         Debug.Log("Animación de ataque terminada");
-    }
-
-    private void OnDrawGizmos()
-    {
-        // Dibujar el alcance del ataque en el editor para visualización
-        if (hitSpot != null)
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(hitSpot.position, range);
-        }
     }
 }
